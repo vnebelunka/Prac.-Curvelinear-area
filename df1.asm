a96029b008283562597fe7fd39fc1c8bd1a0ecb7
vnebelunka <vnebelunka@gmail.com>
2021-05-06 15:24:31 +0300

    comments added

--- df1.asm
+++ df1.asm
@@ -1,26 +1,27 @@
+; exp(x)
 section .text
 global df1
-df1:; -exp(-x)
+df1:
     push ebp
     mov ebp, esp
     finit   
     fld qword[ebp + 8]
     fldl2e
-    fmulp
-    frndint
-    fld1
-    fscale
+    fmulp ; log_2(e) * x
+    frndint ; log_2(e) * x rounded
+    fld1 ; for fscale: st(0) * 2^(st(1))
+    fscale ; 2^(log_2(e) * x) = e ^ x rounded
+    fld qword[ebp + 8]
+    fldl2e
+    fmulp ; log_2(e) * x
     fld qword[ebp + 8]
     fldl2e
     fmulp
-    fld qword[ebp + 8]
-    fldl2e
-    fmulp
-    frndint
-    fsubp
+    frndint; log_2(e) * x rounded
+    fsubp ; log_2(e)*x(true - rounded)
     f2xm1
     fld1
-    fadd
-    fmulp
+    fadd ; e^x(true - rounded)
+    fmulp ; e^x
     leave
     ret
--- f1.asm
+++ f1.asm
@@ -7,25 +7,25 @@ f1:
     finit   
     fld qword[ebp + 8]
     fldl2e
-    fmulp
-    frndint
-    fld1
-    fscale
+    fmulp ; log_2(e) * x
+    frndint ; log_2(e) * x rounded
+    fld1 ; for fscale: st(0) * 2^(st(1))
+    fscale ; 2^(log_2(e) * x) = e ^ x rounded
+    fld qword[ebp + 8]
+    fldl2e
+    fmulp ; log_2(e) * x
     fld qword[ebp + 8]
     fldl2e
     fmulp
-    fld qword[ebp + 8]
-    fldl2e
-    fmulp
-    frndint
-    fsubp
+    frndint; log_2(e) * x rounded
+    fsubp ; log_2(e)*x(true - rounded)
     f2xm1
     fld1
-    fadd
-    fmul
+    fadd ; e^x(true - rounded)
+    fmul ; e^x
     fld1
     fadd
     fld1
-    faddp
+    faddp ; e^x + 2
     leave
     ret
