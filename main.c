58c8e2fd75f0628e7528766445c6d0d8103db3c3
vnebelunka <vnebelunka@gmail.com>
2021-05-06 14:48:43 +0300

    iters updated

--- main.c
+++ main.c
@@ -37,15 +37,21 @@ int main(int argc, char **argv){
 		}
 	}
 	double r1 = root(f1, df1, f2, df2, -1, -0.1, 1e-5);//first root; e^x + 2 = -1/x
+	if(flagiters){
+    	printf("Iters for 'f1=f2': %u\n", iters);
+    }
     double r2 = root(f2, df2, f3, df3, -2.5, -1, 1e-5);//second root; -1/x = -2(x+1)/3
+    if(flagiters){
+    	printf("Iters for 'f2=f3': %u\n", iters);
+    }
     double r3 = root(f1, df1, f3, df3, -5, -3, 1e-5);//third root; e^x + 2 = -2(x+1)/3
+    if(flagiters){
+    	printf("Iters for 'f1=f3': %u\n", iters);
+    }
    	double s1 = integral(f1, r3, r1, 1e-5);//first integral; e^x + 2 from r3 to r1
     double s2 = integral(f2, r2, r1, 1e-5);//second integral;-1/x from r2 to r1
     double s3 = integral(f3, r3, r2, 1e-5);//third integral; -2(x+1)/3 from r3 to r2
     printf("Area is %lf\n", s1-s2-s3);//area formula
-    if(flagiters){
-    	printf("Total iters: %u\n", iters);
-    }
     if(flagroots){
     	printf("Roots are:\nf1 = f2:\t%.8lf\nf2 = f3:\t%.8lf\nf3 = f1:\t%.8lf\n", r1, r2,r3);
     }
--- root.c
+++ root.c
@@ -7,6 +7,7 @@ extern unsigned iters;
 
 double root(double (*f1)(double), double (*df1)(double), double (*f2)(double), double(*df2)(double),
 			double a, double b, double eps){
+	iters = 0;
 	double x_1 = (a + b) / 2;
 	while(fabs(f1(x_1) - f2(x_1)) >= eps / 2){
 		x_1 = x_1 - (f1(x_1) - f2(x_1)) / (df1(x_1) - df2(x_1));
